a5e7ef0557a7d77eff1807a4861a4b0f0161dcdd
BRITANNIA FORGE <britanniaforge@gmail.com>
2025-07-18 08:57:06 +0200

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'),
-      '@shared': path.resolve('../shared')
-    }
-  },
+  plugins: [react(), tsconfigPaths()],
   build: {
     outDir: 'dist',
     sourcemap: true

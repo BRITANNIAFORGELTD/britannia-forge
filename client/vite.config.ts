edf081911834e8da11e3b26cf3b535e9036d5a59
BRITANNIA FORGE <britanniaforge@gmail.com>
2025-07-18 18:17:19 +0200

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
@@ -11,5 +12,11 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@assets': resolve(__dirname, 'src/assets')
+    }
   }
 })
